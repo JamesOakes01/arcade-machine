5d0cd9b16f59441ab6fa7e81e19437dc91f8d89d
original-name-creator <lukestoff@icloud.com>
2024-11-05 17:30:32 -0600

    improves autoupdate

--- selfUpdater.py
+++ selfUpdater.py
@@ -2,24 +2,17 @@ import os
 import git
 from git import Repo
 
-repo_path = os.path.dirname(os.path.abspath(__file__))
-
-
 def update_from_git(repo_path, branch="main"):
     """Updates the code from a Git repository."""
 
     try:
         repo = Repo(repo_path)
-        if repo.is_dirty():
-            print("Repository has uncommitted changes. Please commit or stash them before updating.")
-            return False
-
         origin = repo.remotes.origin
         origin.fetch()
 
         if repo.active_branch.name != branch:
             print(f"Switching to branch {branch}")
-            repo.git.checkout(branch)
+            repo.git.checkout(branch, force = True)
 
         print("Pulling changes from remote...")
         origin.pull()
@@ -31,7 +24,6 @@ def update_from_git(repo_path, branch="main"):
         print(f"Error updating from Git: {e}")
         return False
 
-if __name__ == "__main__":
-    repo_path = "."  # Replace with the actual path to your repository
-    update_from_git(repo_path)
+repo_path = os.path.dirname(os.path.abspath(__file__))
+update_from_git(repo_path)
 
